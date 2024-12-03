2c3fe8219dc7060bea7bb6a823537b3e4ab801f7
Nikolay Lapshin <nlapshin1989@gmail.com>
2024-12-03 19:20:12 +0300

    Commit

--- 2024-10/07 - express-features/index.js
+++ 2024-10/07 - express-features/index.js
@@ -1,117 +1,69 @@
 import express from 'express';
-import { router as corsRouter } from './routes/cors.js';
 import bodyParser from 'body-parser';
-import cors from 'cors';
-import basicAuth from 'express-basic-auth';
+import cookieParser from 'cookie-parser';
 import fs from 'fs';
-import xmlparser from 'express-xml-bodyparser';
-import { render } from 'pug';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 
-const users = [
-    { name: 'user1', pwd: 'password@@@' }
-];
+// Настройка шаблонизатора (Pug)
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'));
 
+// Middlewares
+app.use(bodyParser.json()); // Для обработки JSON
+app.use(bodyParser.urlencoded({ extended: true })); // Для обработки URL-encoded данных
+app.use(cookieParser()); // Для обработки cookies
 
-app.use(cors());
-// app.use(basicAuth({
-//     users: { 'user1': 'password@@@' }
-// }))
+// Маршрут для теста парсеров Body и Cookie
+app.post('/submit', (req, res) => {
+  const { name, message } = req.body; // Парсинг тела запроса
+  const userCookie = req.cookies.user || 'Гость'; // Парсинг cookie
 
+  res.render('response', {
+    name,
+    message,
+    user: userCookie,
+  });
+});
+
+// Установка cookie
+app.get('/set-cookie', (req, res) => {
+  res.cookie('user', 'TestUser', { httpOnly: true });
+  res.send('Cookie установлена!');
+});
+
+// Маршрут для отображения HTML через шаблонизатор
+app.get('/', (req, res) => {
+  res.render('index', { title: 'Express Example', message: 'Welcome to Express!' });
+});
+
+// Маршрут для отправки файла через потоки
+app.get('/download', (req, res) => {
+  const filePath = path.join(__dirname, 'files', 'example.txt'); // Убедитесь, что файл существует
+  const fileStream = fs.createReadStream(filePath);
+
+  res.setHeader('Content-Disposition', 'attachment; filename="example.txt"');
+  fileStream.pipe(res); // Отправка файла через потоки
+});
+
+// 404 Middleware
 app.use((req, res, next) => {
-
-
-
-
-    req.on('data', (da) => {
-        req.body = JSON.parse(da.toString());
-        // console.log(da.toString());
-    });
-    req.on('end', () => {
-        next();
-    });
+  res.status(404).render('404', { message: 'Page not found' });
 });
 
-// app.use((req, res, next) => {
-//     console.log(req.query);
-//     console.log('inside 1st middleware');
-//     res.on('finish', () => {
-//         console.log('middleware 1 finish');
-//     })
-//     next();
-
-// });
-
-
-// app.set('view engine', 'pug');
-//app.use(bodyParser.json());
-//app.use(xmlparser());
-app.use('/cors', corsRouter);
-// app.use(cors())
-
-// app.use((req, res, next) => {
-
-//     const b64auth = (req.headers.authorization || '').split(' ')[1] || '';
-//     console.log(Buffer.from(b64auth, 'base64').toString());
-//     const [login, password] = Buffer.from(b64auth, 'base64').toString().split(':');
-// console.log('login',login, 'passwd',password);
-//     if (users.some(x => x.name === login && x.pwd === password)) {
-
-//         return next();
-//     }
-
-//     res.set('WWW-Authenticate', 'Basic realm="401"') // change this
-//     res.status(401).send('Authentication required.') // custom message  
-// });
-
-
-app.get('/users', (_, res) => {
-    console.log('inside request');
-    res.send('ok1');
-    console.log('after request');
-})
-
-app.post('/users', (req, res) => {
-    console.log(req.body);
-
-    res.send('ok');
+// Обработка ошибок
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Что-то пошло не так!');
 });
 
-app.post('/xml', (req, res) => {
-    console.log(JSON.stringify(req.body));
-
-    res.send('ok');
+// Запуск сервера
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Сервер запущен на http://localhost:${PORT}`);
 });
-
-app.get('/pug', (req, res) => {
-    res.render('index', { list1: ['alpha', 'beta', 'gamma'] ,msg:'aaa маленькое'});
-});
-
-
-app.engine('mytpl', (filePath, options, callback) => {
-    fs.readFile(filePath, (err, content) => {
-        if (err) return callback(err)
-        let rendered = content.toString();
-
-        console.log('tpl: ',rendered);
-        console.log('options: ',options);
-        for (let o in options.params) {
-            rendered = rendered.replace("#{" + o + "}", options.params[o]);
-        }
-
-
-        console.log(rendered);
-
-        return callback(null, rendered)
-    })
-});
-app.set('view engine','pug');
-
-app.set('views', './views');
-
-app.get('/mytpl-demo', (req,res)=>{
-    res.render('my',{params:{ title:' УРАААААА', username:'otus-user'}});
-});
-
-app.listen(3001, () => { });
