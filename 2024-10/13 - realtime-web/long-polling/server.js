f069c1db69a5174338e49e4c3f6d856151a92799
Nikolay Lapshin <nlapshin1989@gmail.com>
2024-12-24 19:08:57 +0300

    Added examples

---
+++ 2024-10/13 - realtime-web/long-polling/server.js
@@ -0,0 +1,30 @@
+const { setTimeout: delay } = require('node:timers/promises');
+
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+app.use(cors());
+
+let dataQueue = [];
+
+// Генерация новых данных (например, события или обновления)
+setInterval(() => {
+    const timestamp = new Date().toISOString();
+    dataQueue.push(`Новые данные на ${timestamp}`);
+}, 5000); // Новые данные каждые 5 секунд
+
+// Эндпоинт для длинного опроса
+app.get('/long-poll', async(req, res) => {
+    while(true) {
+        if (dataQueue.length) {
+            return res.json({ success: true, data: dataQueue.shift() });
+        }
+
+        await delay(1000);
+    };
+});
+
+app.listen(3000, () => {
+    console.log('Сервер запущен на http://localhost:3000');
+});
