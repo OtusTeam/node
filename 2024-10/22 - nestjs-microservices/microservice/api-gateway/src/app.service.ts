0430d52ceac61f4ef401c8e52202a6439e6d13a8
Nikolay Lapshin <nlapshin1989@gmail.com>
2025-02-04 19:35:47 +0300

    Commit

---
+++ 2024-10/22 - nestjs-microservices/microservice/api-gateway/src/app.service.ts
@@ -0,0 +1,41 @@
+import { Injectable, Inject } from '@nestjs/common';
+import { ClientProxy } from '@nestjs/microservices';
+import { lastValueFrom } from 'rxjs';
+
+@Injectable()
+export class AppService {
+  constructor(
+    @Inject('MAILER_SERVICE') private readonly mailerService: ClientProxy,
+  ) {}
+ 
+  // Создание юзера на сервисе А
+  // commit method и rollback method
+  
+  async createUser(user: any){
+    const pattern = { cmd: 'user-create' };
+    const payload = user;
+
+    // Второй сервис, который создает юзеров сам
+    // Третий сервис, нужно положить аналитическую БД этого юзера.
+
+    // pattern - это способ подписать событие
+    // payload - это полезная нагрузка
+    // this.mailerService.send<any>(pattern, payload);
+
+    // Observable - rxjs
+
+    console.log('service API Gateway. Handle user', user);
+
+    const mailRes = await lastValueFrom(this.mailerService.send<any>(pattern, payload));
+
+    console.log('service API Gateway. Handle response', mailRes);
+
+    return { success: true };
+  }
+}
+
+// Pub-Sub
+// Один публикует событие. Но не важно, что его прочитали
+
+// Сlient-Server
+// Я публикую событие и жду ответа.
