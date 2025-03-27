e403cc09cfdf36e7efc3abf1de4470cea53ec461
Nikolay Lapshin <nlapshin1989@gmail.com>
2025-03-27 21:43:39 +0300

    commit

--- 2025-03/03 - events-loop/examples/event-loop/01-micro-macro-tasks.js
+++ 2025-03/03 - events-loop/examples/event-loop/01-micro-macro-tasks.js
@@ -1,23 +1,32 @@
-console.log('script start');
+console.log('script start'); // 1
 
 setTimeout(function () {
   console.log('setTimeout');
-}, 0)
+}, 0) // 6
 
 setImmediate(() => {
   console.log('setImmediate');
-})
+}) // 7
 
 process.nextTick(() => {
   console.log('nextTick');
-})
+}) // 3
 
 Promise.resolve()
   .then(function () {
-    console.log('promise1');
+    console.log('promise1'); // 4
   })
   .then(function () {
-    console.log('promise2');
+    console.log('promise2');  // 5
   })
 
-console.log('script end');
+console.log('script end'); // 2
+
+
+// script start
+// script end
+// nextTick
+// promise1
+// promise2
+// setTimeout
+// setImmediate
